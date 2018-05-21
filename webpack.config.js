277fada63392ceb5443cc492e814a1b757696fd3
bmagnantb <bmagnantb@gmail.com>
2018-05-20 21:07:18 -0400

    adds webpack-dev-server w/ hot-reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
+const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    path: __dirname + '/build',
-    filename: 'bundle.js'
-  },
+  output: { path: __dirname + '/build', filename: 'bundle.js' },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin('./build'),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       filename: 'index.html'
-    })
-  ]
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: { hot: true, overlay: true }
 }
